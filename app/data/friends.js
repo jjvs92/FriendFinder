f53dd98b1f16b09c71321ff585c01d771be20f6e
jjvs92 <julian_jvs92@hotmail.com>
2018-07-08 21:39:34 -0500

    Completed working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var listOfFriends= [{
     name: "Ahmed",
-    photo: "PlaceHolder",
+    photo: "https://dmxvlyap9srmn.cloudfront.net/production/avatars/74/Mike-01.jpg",
     scores:[5, 3, 2, 4, 5, 1, 1, 4, 4, 3]
 },{
     name: "Angel",
-    photo: "placeholder", 
+    photo: "https://imgix.bustle.com/rehost/2016/9/13/7c01b759-85c6-49db-ab21-e6f080a63a8b.png?w=970&h=582&fit=crop&crop=faces&auto=format&q=70", 
     scores: [5, 3, 3, 1, 1, 2, 3, 5, 3, 2]
 },{
     name: "Luis",
-    photo: "placeholder",
+    photo: "https://www.unilad.co.uk/wp-content/uploads/2017/02/14103UNILAD-imageoptim-mike-myers-teeth-austin-powers.jpg",
     scores: [4, 3, 3, 2, 1, 4, 3, 5, 3, 2]
 },{
     name: "Jose",
-    photo: "placeholder",
+    photo: "https://www.heritagemedical.com/images/uploads/physicians/_thb/Mike_Smith.jpeg",
     scores: [2, 3, 4, 5, 5, 2, 2, 1, 3, 2]
 }]
 
